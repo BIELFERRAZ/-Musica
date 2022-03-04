c32604dfa26f14a587582eb19f854520d66d079d
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-03-04 18:26:19 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Ousadia e alegria");
+Console.ResetColor();
+Console.WriteLine("Chego chegando, beijo no canto da boca");
+Console.WriteLine("Tumultuando, ai meu Deus que coisa louca");
+Console.WriteLine("Se der espaço, eu pedalo e vou pra cima");
+Console.WriteLine("Corro pro abraço, vacilo entro no clima.");
+Console.WriteLine();
+Console.WriteLine("O nosso lema é ousadia e alegria");
+Console.WriteLine("nossa cara é pagode todo dia");
+Console.WriteLine("Cheio de estilo na pressão eu vou com tudo");
+Console.WriteLine("Só espalhando ousadia pelo o mundo.");
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Lê lêlê, lêlê, lêlê");
+Console.WriteLine("Ousadia pra vencer");
+Console.WriteLine("Lê lêlê, lêlê, lêlê");
+Console.WriteLine("Alegria pra viver.");
+
+
